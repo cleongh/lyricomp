32643ccf1ba602c677ca5ec600da1ff85bdd4ea4
Carlos León <cleon@ucm.es>
2024-02-08 10:21:31 +0100

    initial version

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # lyricomp
+
+For installing dependencies:
+
+```sh
+pip install -r requirements.txt
+```
---
+++ lyricomp.py
@@ -0,0 +1,6 @@
+from pyverse import Pyverse
+
+poema = 'con cien cañones por banda viento en popa a toda vela no corta el mar sino vuela'
+
+verse = Pyverse(poema)
+print(verse.syllables)
