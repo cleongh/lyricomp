7eb4b350d07eb23c80459603fecbf31315849b35
Carlos León <cleon@ucm.es>
2024-07-03 16:09:28 +0200

    pruebas con aima y partitura

--- lyricomp.py
+++ lyricomp.py
@@ -1,32 +1,105 @@
-from pyverse import Pyverse
+# from pyverse import Pyverse
 
-def extract_verse(syllables, poem):
-    return poem[:syllables], poem[syllables:]
+# def extract_verse(syllables, poem):
+#     return poem[:syllables], poem[syllables:]
     
-def versification_bt(poem):
-    if poem == []:
-        yield []
-    else:
-        for syllables in [8, 11, 16]:
-            if len(poem) >= syllables:
-                current_verse, rest = extract_verse(syllables, poem)
-                for candidate_versification in versification_bt(rest):
-                    yield [current_verse] + candidate_versification
+# def versification_bt(poem):
+#     if poem == []:
+#         yield []
+#     else:
+#         for syllables in [8, 11, 16]:
+#             if len(poem) >= syllables:
+#                 current_verse, rest = extract_verse(syllables, poem)
+#                 for candidate_versification in versification_bt(rest):
+#                     yield [current_verse] + candidate_versification
 
-def versification(poem):
-    return versification_bt(list(filter(lambda x: x != '', Pyverse(poem).syllables.split('-'))))
+# def versification(poem):
+#     return versification_bt(list(filter(lambda x: x != '', Pyverse(poem).syllables.split('-'))))
 
-def beautify_poem(poem_list):
-    poem = ''
-    for verse in poem_list:
-        poem += ' '.join(verse)
-        poem += '\n'
-    return poem
+# def beautify_poem(poem_list):
+#     poem = ''
+#     for verse in poem_list:
+#         poem += ' '.join(verse)
+#         poem += '\n'
+#     return poem
 
-i = 0
-for candidate_versification in versification('con cien cañones por banda viento en popa a toda vela no corta el mar sino vuela'):
-    print('versification ', i)
-    i += 1
-    print(beautify_poem(candidate_versification))
-    print('-' * 15)
-    
+# i = 1
+# for candidate_versification in versification('con cien cañones por banda viento en popa a toda vela no corta el mar sino vuela'):
+#     print('versification ', i)
+#     i += 1
+#     print(beautify_poem(candidate_versification))
+#     print('-' * 15)
+
+
+# https://github.com/CPJKU/partitura
+
+# import partitura as pt
+# import numpy as np
+
+# # my_xml_file = "example.mei" # pt.EXAMPLE_MUSICXML
+# my_xml_file = pt.EXAMPLE_MEI
+
+# score = pt.load_score(my_xml_file)
+
+
+
+# part = score.parts[0]
+
+# pianoroll = np.array([(n.start.t, n.end.t, n.midi_pitch) for n in part.notes])
+
+# print(part.pretty())
+
+# beat_map = part.beat_map
+
+# print(beat_map(pianoroll[:, 0]))
+
+
+import aima3.search
+
+# from aima3.search import backtracking_search, NQueensProblem
+
+# aima.csp.backtracking_search(syllables)
+
+class State:
+    def __init__(self, original):
+        self.original = original
+        self.verses = [[]]
+
+class Syllables(aima3.search.Problem):
+    def __init__(self, initial, goal=None):
+        super().__init__(State(list(reversed(initial))),goal)
+
+    def actions(self, state):
+        actions = [x for x in range(len(state.verses)) if len(state.verses[x]) <= 8]
+        return actions
+        
+
+    def result(self, state, action):
+        # Return the new state after applying the action
+        syllable = state.original.pop()
+        if len(state.verses[action]) == 0:
+            state.verses[action] = [syllable]
+        else:
+            state.verses[action].append(syllable)
+        return state
+
+    def goal_test(self, state):
+        # Check if the state is the goal state
+        return len(state.original) == 0
+
+    def path_cost(self, c, state1, action, state2):
+        # Define the path cost function
+        return c + 1  # Example: Incremental cost of 1 per step
+
+    def value(self, state):
+        # Define a value function for optimization problems
+        return 0
+
+
+problem=Syllables(['en', 'un', 'lu', 'gar'])
+solution_node = aima3.search.breadth_first_search(problem)
+if solution_node:
+    print("Solution path:", solution_node.solution())
+    print("Path cost:", solution_node.path_cost)
+else:
+    print("No solution found")
